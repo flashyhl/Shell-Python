e3dc8a1cf596ba51bf1fb7e52dfbe5eccd87ea78
flashyhl <flashylh@hotmail.com>
2016-04-06 11:11:16 +0800

    upload folder:auto_refresh_aliyun_cdn

---
+++ python/auto_refresh_aliyun_cdn/auto_cdn_refresh.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import sys,os
+import urllib, urllib2
+import base64
+import hmac
+import hashlib
+from hashlib import sha1
+import time
+import uuid
+import json
+from optparse import OptionParser
+import ConfigParser
+import traceback
+
+access_key_id = '';
+access_key_secret = '';
+cdn_server_address = 'https://cdn.aliyuncs.com'
+CONFIGFILE = os.getcwd() + '/config/.aliyuncredentials.ini'
+cdn_file = os.getcwd() + '/config/cdn_url'
+CONFIGSECTION = 'Credentials'
+
+def percent_encode(str):
+    res = urllib.quote(str.decode(sys.stdin.encoding).encode('utf8'), '')
+    res = res.replace('+', '%20')
+    res = res.replace('*', '%2A')
+    res = res.replace('%7E', '~')
+    return res
+
+def compute_signature(parameters, access_key_secret):
+    sortedParameters = sorted(parameters.items(), key=lambda parameters: parameters[0])
+
+    canonicalizedQueryString = ''
+    for (k,v) in sortedParameters:
+        canonicalizedQueryString += '&' + percent_encode(k) + '=' + percent_encode(v)
+
+    stringToSign = 'GET&%2F&' + percent_encode(canonicalizedQueryString[1:])
+
+    h = hmac.new(access_key_secret + "&", stringToSign, sha1)
+    signature = base64.encodestring(h.digest()).strip()
+    return signature
+
+def compose_url(user_params):
+    timestamp = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
+
+    parameters = { \
+            'Format'        : 'JSON', \
+            'Version'       : '2014-11-11', \
+            'AccessKeyId'   : access_key_id, \
+            'SignatureVersion'  : '1.0', \
+            'SignatureMethod'   : 'HMAC-SHA1', \
+            'SignatureNonce'    : str(uuid.uuid1()), \
+            'TimeStamp'         : timestamp, \
+    }
+
+    for key in user_params.keys():
+        parameters[key] = user_params[key]
+
+    signature = compute_signature(parameters, access_key_secret)
+    parameters['Signature'] = signature
+    url = cdn_server_address + "/?" + urllib.urlencode(parameters)
+    return url
+
+def make_request(user_params, quiet=False):
+    url = compose_url(user_params)
+    #print url
+    request = urllib2.Request(url)
+
+    try:
+        conn = urllib2.urlopen(request)
+        response = conn.read()
+    except urllib2.HTTPError, e:
+        print(e.read().strip())
+        raise SystemExit(e)
+
+    #make json output pretty, this code is copied from json.tool
+    try:
+        obj = json.loads(response)
+        if quiet:
+            return obj
+    except ValueError, e:
+        raise SystemExit(e)
+    json.dump(obj, sys.stdout, sort_keys=True, indent=2)
+    sys.stdout.write('\n')
+
+def setup_credentials():
+    config = ConfigParser.ConfigParser()
+    try:
+        config.read(CONFIGFILE)
+        global access_key_id
+        global access_key_secret
+        access_key_id = config.get(CONFIGSECTION, 'accesskeyid')
+        access_key_secret = config.get(CONFIGSECTION, 'accesskeysecret')
+    except Exception, e:
+		print traceback.format_exc()
+		print("can't get access key pair, use <manual_cdn_refresh.py  config --id=[accesskeyid] --secret=[accesskeysecret]> to setup")
+		sys.exit(1)
+
+
+
+if __name__ == '__main__':
+    user_params = {}
+    setup_credentials()
+    user_params['action'] = 'PushObjectCache'
+    read_cdn_file = open(cdn_file, "r")
+    for read_one_row in read_cdn_file:
+        user_params['ObjectPath'] = read_one_row.strip()
+        #print user_params
+        make_request(user_params)
+        #print user_params
+    read_cdn_file.close()
+
